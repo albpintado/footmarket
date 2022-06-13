12b07b06bc8613cd0bb62f4990676fce37e139d0
Alberto Pintado <albertopintadolozano@gmail.com>
2022-06-13 20:25:32 +0200

    Add responsive media queries

--- src/App.tsx
+++ src/App.tsx
@@ -52,46 +52,48 @@ function App() {
           </a>
         </nav>
       </header>
-      {transfers.map((transfer: ClientTransfer) => {
-        return (
-          <article
-            key={`${transfer.playerId} from ${transfer.teams.out.name} to ${transfer.teams.in.name} in ${transfer.date}`}
-          >
-            <section className="player-name">
-              <h2>{transfer.playerName}</h2>
-            </section>
-            <section className="transfer-info">
-              <ul>
-                <li>{transfer.date.toLocaleString()}</li>
-                <li>{transfer.type}</li>
-              </ul>
-              <div className="team-info">
-                <header>
-                  <p>Baja</p>
-                  <img
-                    src={transfer.teams.out.logo || ""}
-                    alt={`Logo de ${transfer.teams.out.name}`}
-                    className="team-logo"
-                  />
-                </header>
-                <footer>{transfer.teams.out.name}</footer>
-              </div>
-              <i className="fa-solid fa-right-long"></i>
-              <div className="team-info">
-                <header>
-                  <p>Alta</p>
-                  <img
-                    src={transfer.teams.in.logo || ""}
-                    alt={`Logo de ${transfer.teams.in.name}`}
-                    className="team-logo"
-                  />
-                </header>
-                <footer>{transfer.teams.in.name}</footer>
-              </div>
-            </section>
-          </article>
-        );
-      })}
+      <div className="container">
+        {transfers.map((transfer: ClientTransfer) => {
+          return (
+            <article
+              key={`${transfer.playerId} from ${transfer.teams.out.name} to ${transfer.teams.in.name} in ${transfer.date}`}
+            >
+              <section className="player-name">
+                <h2>{transfer.playerName}</h2>
+              </section>
+              <section className="transfer-info">
+                <ul>
+                  <li>{transfer.date.toLocaleString()}</li>
+                  <li>{transfer.type}</li>
+                </ul>
+                <div className="team-info">
+                  <header>
+                    <p>Baja</p>
+                    <img
+                      src={transfer.teams.out.logo || ""}
+                      alt={`Logo de ${transfer.teams.out.name}`}
+                      className="team-logo"
+                    />
+                  </header>
+                  <footer>{transfer.teams.out.name}</footer>
+                </div>
+                <i className="fa-solid fa-right-long"></i>
+                <div className="team-info">
+                  <header>
+                    <p>Alta</p>
+                    <img
+                      src={transfer.teams.in.logo || ""}
+                      alt={`Logo de ${transfer.teams.in.name}`}
+                      className="team-logo"
+                    />
+                  </header>
+                  <footer>{transfer.teams.in.name}</footer>
+                </div>
+              </section>
+            </article>
+          );
+        })}
+      </div>
       <footer>
         <p>Betisdiario - Alberto Pintado</p>
       </footer>
