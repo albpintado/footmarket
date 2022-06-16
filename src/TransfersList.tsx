66c045e6610c8a38c53f72fcd8101e866b02d886
Alberto Pintado <albertopintadolozano@gmail.com>
2022-06-16 16:38:22 +0200

    Convert each transfer article in a React component

--- src/TransfersList.tsx
+++ src/TransfersList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ClientTransfer } from "Interfaces/ClientTransfer";
+import Transfer from "Transfer";
 
 type TransfersListProps = {
   transfers: ClientTransfer[];
@@ -15,86 +16,18 @@ const TransfersList = ({
       {transfers.map((transfer: ClientTransfer) => {
         if (season === transfer.season) {
           return (
-            <article
+            <Transfer
+              transfer={transfer}
               key={`${transfer.playerId} from ${transfer.teams.out.name} to ${transfer.teams.in.name} in ${transfer.date}`}
-            >
-              <section className="player-name">
-                <h2>{transfer.playerName}</h2>
-              </section>
-              <section className="transfer-info">
-                <ul>
-                  <li>{transfer.date.toLocaleString()}</li>
-                  <li>{transfer.type}</li>
-                </ul>
-                <div className="team-info">
-                  <header>
-                    <p>Baja</p>
-                    <img
-                      src={transfer.teams.out.logo || ""}
-                      alt={`Logo de ${transfer.teams.out.name}`}
-                      className="team-logo"
-                    />
-                  </header>
-                  <footer>{transfer.teams.out.name}</footer>
-                </div>
-                <i className="fa-solid fa-right-long"></i>
-                <div className="team-info">
-                  <header>
-                    <p>Alta</p>
-                    <img
-                      src={transfer.teams.in.logo || ""}
-                      alt={`Logo de ${transfer.teams.in.name}`}
-                      className="team-logo"
-                    />
-                  </header>
-                  <footer>{transfer.teams.in.name}</footer>
-                </div>
-              </section>
-            </article>
+            />
           );
         }
         if (season === "Todos") {
           return (
-            <article
+            <Transfer
+              transfer={transfer}
               key={`${transfer.playerId} from ${transfer.teams.out.name} to ${transfer.teams.in.name} in ${transfer.date}`}
-            >
-              <section className="player-name">
-                <h2>{transfer.playerName}</h2>
-              </section>
-              <section className="transfer-info">
-                <ul>
-                  <li>{transfer.date.toLocaleString()}</li>
-                  <li>{transfer.type}</li>
-                </ul>
-                <div className="team-info">
-                  <header>
-                    <p>Baja</p>
-                    <img
-                      src={transfer.teams.out.logo || ""}
-                      alt={`Logo de ${transfer.teams.out.name}`}
-                      className="team-logo"
-                    />
-                  </header>
-                  <footer className="transfer-team-name">
-                    {transfer.teams.out.name}
-                  </footer>
-                </div>
-                <i className="fa-solid fa-right-long"></i>
-                <div className="team-info">
-                  <header>
-                    <p>Alta</p>
-                    <img
-                      src={transfer.teams.in.logo || ""}
-                      alt={`Logo de ${transfer.teams.in.name}`}
-                      className="team-logo"
-                    />
-                  </header>
-                  <footer className="transfer-team-name">
-                    {transfer.teams.in.name}
-                  </footer>
-                </div>
-              </section>
-            </article>
+            />
           );
         }
         return;
