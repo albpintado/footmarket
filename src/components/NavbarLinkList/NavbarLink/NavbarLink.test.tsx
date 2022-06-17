b797046e3fdf749ab5873ca7923e72016b9b4238
Alberto Pintado <albertopintadolozano@gmail.com>
2022-06-17 17:42:26 +0200

    Add test for Todos NavbarLink

--- src/components/NavbarLinkList/NavbarLink/NavbarLink.test.tsx
+++ src/components/NavbarLinkList/NavbarLink/NavbarLink.test.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { render, screen, waitFor, within } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Router } from "react-router-dom";
 import { createMemoryHistory } from "history";
 import NavbarLink from "./NavbarLink.component";
 import userEvent from "@testing-library/user-event";
 
-describe("App", () => {
-  it("renders Betisdiario h1", () => {
+describe("NavbarLink", () => {
+  it("shows a link with Todos text and drives to /", () => {
+    const history = createMemoryHistory({ initialEntries: ["/2023"] });
+    render(
+      <Router location={history.location} navigator={history}>
+        <NavbarLink season="Todos" />
+      </Router>
+    );
+
+    const link = screen.getByRole("link", { name: /todos/i });
+    userEvent.click(link);
+
+    expect(link).toBeInTheDocument();
+    expect(history.location.pathname).toBe("/");
+  });
+
+  it("shows a link with 2022/23 text and drives to /2023", () => {
     const history = createMemoryHistory({ initialEntries: ["/"] });
     render(
       <Router location={history.location} navigator={history}>
