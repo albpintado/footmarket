3a9e0c8fcdf0080519390f11939fa99258029fdd
Alberto Pintado <albertopintadolozano@gmail.com>
2022-06-23 11:42:46 +0200

    Modify TransfersList to render filtered Transfers

--- src/components/TransfersList/TransfersList.component.tsx
+++ src/components/TransfersList/TransfersList.component.tsx
@@ -5,12 +5,68 @@ import Transfer from "./Transfer/Transfer.component";
 type TransfersListProps = {
   transfers: ClientTransfer[];
   season: string;
+  filterQuery: string;
 };
 
 const TransfersList = ({
   transfers,
   season,
+  filterQuery,
 }: TransfersListProps): JSX.Element => {
+  const filteredTransfers = transfers.filter((transfer: ClientTransfer) =>
+    transfer.playerName
+      ?.toLowerCase()
+      .normalize("NFKD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .includes(filterQuery.normalize("NFC").replace(/[\u0300-\u036f]/g, ""))
+  );
+
+  return filterQuery === "" ? (
+    <>
+      {transfers.map((transfer: ClientTransfer) => {
+        if (season === transfer.season) {
+          return (
+            <Transfer
+              transfer={transfer}
+              key={`${transfer.playerId} from ${transfer.teams.out.name} to ${transfer.teams.in.name} in ${transfer.date}`}
+            />
+          );
+        }
+        if (season === "Todos") {
+          return (
+            <Transfer
+              transfer={transfer}
+              key={`${transfer.playerId} from ${transfer.teams.out.name} to ${transfer.teams.in.name} in ${transfer.date}`}
+            />
+          );
+        }
+        return;
+      })}
+    </>
+  ) : (
+    <>
+      {filteredTransfers.map((transfer: ClientTransfer) => {
+        if (season === transfer.season) {
+          return (
+            <Transfer
+              transfer={transfer}
+              key={`${transfer.playerId} from ${transfer.teams.out.name} to ${transfer.teams.in.name} in ${transfer.date}`}
+            />
+          );
+        }
+        if (season === "Todos") {
+          return (
+            <Transfer
+              transfer={transfer}
+              key={`${transfer.playerId} from ${transfer.teams.out.name} to ${transfer.teams.in.name} in ${transfer.date}`}
+            />
+          );
+        }
+        return;
+      })}
+    </>
+  );
+
   return (
     <>
       {transfers.map((transfer: ClientTransfer) => {
--- src/components/TransfersList/TransfersList.test.tsx
+++ src/components/TransfersList/TransfersList.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import { Router } from "react-router-dom";
 import { createMemoryHistory } from "history";
 import transfers from "data/transfersForTests.json";
@@ -12,7 +12,7 @@ describe("TransfersList", () => {
     const history = createMemoryHistory({ initialEntries: ["/"] });
     render(
       <Router location={history.location} navigator={history}>
-        <TransfersList transfers={allTransfers} season="Todos" />
+        <TransfersList transfers={allTransfers} season="Todos" filterQuery="" />
       </Router>
     );
     const articles = screen.getAllByRole("article");
@@ -23,10 +23,29 @@ describe("TransfersList", () => {
     const history = createMemoryHistory({ initialEntries: ["/2023"] });
     render(
       <Router location={history.location} navigator={history}>
-        <TransfersList transfers={transfers} season="2023" />
+        <TransfersList transfers={transfers} season="2023" filterQuery="" />
       </Router>
     );
     const articles = screen.getAllByRole("article");
     expect(articles).toHaveLength(5);
+  });
+
+  it.only("shows 2 articles when Todos text is passed and bellerin is the query", () => {
+    const history = createMemoryHistory({ initialEntries: ["/"] });
+    render(
+      <Router location={history.location} navigator={history}>
+        <TransfersList
+          transfers={transfers}
+          season="Todos"
+          filterQuery="bellerin"
+        />
+      </Router>
+    );
+
+    const articles = screen.getAllByRole("article");
+    const playerName = within(articles[0]).getByRole("heading");
+
+    expect(articles).toHaveLength(1);
+    expect(playerName).toHaveTextContent("Héctor Bellerín Moruno");
   });
 });
