c333692244817327803d7579393706b7ae037e5e
Alberto Pintado <albpintado@protonmail.ch>
2025-08-14 19:51:08 +0200

    ci: add vercel analytics component

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "components/App/App.component";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Analytics />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
